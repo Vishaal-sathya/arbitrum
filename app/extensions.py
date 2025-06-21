46a4a4cfa65f73dc6fd2fc9a87b7955b0767fab8
Vishaal S <svishaal67@gmail.com>
2025-06-21 14:26:01 +0530

    updated api

--- app/extensions.py
+++ app/extensions.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 import google.generativeai as genai
-from config import GEMINI_API_KEY
+from config import Config
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
@@ -17,8 +17,9 @@ limiter = Limiter(
     get_remote_address,
     default_limits=["200 per day","50 per hour"]
 )
+config_ = Config()
 
-genai.configure(api_key=GEMINI_API_KEY)
+genai.configure(api_key=config_.GEMINI_API_KEY)
 gemini = genai.GenerativeModel("gemini-2.0-flash")
 
 
